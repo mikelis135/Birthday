b1113fc2e1bda1616f6f00e3eefe801043f2a561
taiwoadebayo <mikelis135@gmail.com>
2020-12-21 19:38:14 +0100

    Imported all the needed libraries for network calls and logging, database, dependency injection, serialization, unit testing...
    Added packages to structure the code
     DATA which contains local and remote data
     DATABASE which is the db
     UI which contains the user interface
    
     Created some (contract) interface classes which will have real and fake implementation, this is to allow easy unit testing from the fakes

--- app/src/main/java/com/birthday/ui/MainActivity.kt
+++ app/src/main/java/com/birthday/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.birthday
+package com.birthday.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.birthday.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
